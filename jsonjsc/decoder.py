f364f22d63c88bc5b51c23508656701ae57d0c62
Cliff Foster <korewananda@gmail.com>
2018-07-08 19:30:21 -0700

    Is super the only thing preventing 2.7 support? Let's see!

--- jsonjsc/decoder.py
+++ jsonjsc/decoder.py
@@ -5,4 +5,4 @@ from jsonjsc.parser import parse
 class JSONCommentDecoder(json.JSONDecoder):
     def decode(self, s, **kwargs):
         s = parse(s)
-        return super().decode(s, **kwargs)
+        return super(JSONCommentDecoder, self).decode(s, **kwargs)
